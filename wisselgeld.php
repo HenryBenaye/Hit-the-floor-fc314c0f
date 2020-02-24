059753f8c0a6eb8b968608fe24a302185851ed10
HENRY BENAYE <2106747@talnet.nl>
2020-02-24 10:55:13 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -4,7 +4,7 @@ $change = (int)$argv[1];
 $coins = [10, 2, 1];
 
 foreach($coins as $munt){
-    if(floor($change / $munt) > 0){
+     if(floor($change / $munt) > 0){
         $times =  floor($change / $munt);
         echo "$times X $munt" . PHP_EOL;
         $change = $change - ($times * $munt);
